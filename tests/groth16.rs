69e2bf16862645c0a7be481cd41bac3c44a5ed7b
ibmp33 <2285673866@qq.com>
2023-10-18 13:33:00 +0800

    chore: reorder groth16 test

--- tests/groth16.rs
+++ tests/groth16.rs
@@ -35,12 +35,30 @@ fn groth16_proof() -> Result<()> {
         "./test-vectors/fibonacci.final.r1cs",
     )?;
     let mut builder = CircomBuilder::new(cfg);
+    
+    //1-setup
+    let start_time0 = Instant::now();
+    let start_time0_1 = Instant::now();
+    // create an empty instance for setting it up
+    let circom = builder.setup();
+    let elapsed_time0_1 = start_time0_1.elapsed();
+    println!("Time taken for builder.setup: {:?}", elapsed_time0_1);
+
+    let start_time0_2 = Instant::now();
+    let mut rng = thread_rng();
+    let params = GrothBn::generate_random_parameters_with_reduction(circom, &mut rng)?;
+    let elapsed_time0_2 = start_time0_2.elapsed();
+    println!("Time taken for GrothBn::generate_random_parameters_with_reduction {:?}", elapsed_time0_2);
+    let elapsed_time0 = start_time0.elapsed();
+    println!("1-(setup) time cost: {:?}", elapsed_time0);
+
+    //2-prove
+    let start_time1 = Instant::now();
+    let start_time1_1 = Instant::now();
     let inputs_path = "./test-vectors/final_input.zkin.json";
     let inputs_str = std::fs::read_to_string(inputs_path).unwrap();
     let inputs: std::collections::HashMap<String, serde_json::Value> =
         serde_json::from_str(&inputs_str).unwrap();
-    
-    let start_time0 = Instant::now();
     let inputs = inputs
         .iter()
         .map(|(key, value)| {
@@ -61,36 +79,34 @@ fn groth16_proof() -> Result<()> {
             builder.push_input(key, value.clone());
         }
     }
-    let elapsed_time0 = start_time0.elapsed();
-    println!("Time taken for builder.push_input: {:?}", elapsed_time0);
+    let elapsed_time1_1 = start_time1_1.elapsed();
+    println!("Time taken for builder.push_input: {:?}", elapsed_time1_1);
 
-    let start_time1 = Instant::now();
-    // create an empty instance for setting it up
-    let circom = builder.setup();
-    let elapsed_time1 = start_time1.elapsed();
-    println!("Time taken for builder.setup: {:?}", elapsed_time1);
-
-    let start_time2 = Instant::now();
-    let mut rng = thread_rng();
-    let params = GrothBn::generate_random_parameters_with_reduction(circom, &mut rng)?;
-    let elapsed_time2 = start_time2.elapsed();
-    println!("Time taken for GrothBn::generate_random_parameters_with_reduction {:?}", elapsed_time2);
-
-    let start_time3 = Instant::now();
+    let start_time1_2 = Instant::now();
     let circom = builder.build()?;
-    let elapsed_time3 = start_time3.elapsed();
-    println!("Time taken for generate witness: {:?}", elapsed_time3);
-
-    let inputs = circom.get_public_inputs().unwrap();
-
-    let start_time = Instant::now();
+    let elapsed_time1_2 = start_time1_2.elapsed();
+    println!("Time taken for generate witness: {:?}", elapsed_time1_2);
+    
+    let start_time1_3 = Instant::now();
     let proof = GrothBn::prove(&params, circom, &mut rng)?;
-    let elapsed_time = start_time.elapsed();
-    println!("Time taken for GrothBn::prove: {:?}", elapsed_time);
+    let elapsed_time1_3 = start_time1_3.elapsed();
+    println!("Time taken for GrothBn::prove: {:?}", elapsed_time1_3);
+    
+    let elapsed_time1 = start_time1.elapsed();
+    println!("2-(prove) time cost: {:?}", elapsed_time1);
 
+    //3-generate verification_key
+    let start_time2 = Instant::now();
     let pvk = GrothBn::process_vk(&params.vk).unwrap();
+    let elapsed_time2 = start_time2.elapsed();
+    println!("3-(generate verification_key) time cost: {:?}", elapsed_time2);
 
+    //4-verify
+    let start_time3 = Instant::now();
+    let inputs = circom.get_public_inputs().unwrap();
     let verified = GrothBn::verify_with_processed_vk(&pvk, &inputs, &proof)?;
+    let elapsed_time3 = start_time3.elapsed();
+    println!("4-(verify) time cost: {:?}", elapsed_time3);
 
     assert!(verified);
 
